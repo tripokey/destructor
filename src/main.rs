8372fb7f2209861ba5ea34ba7fcc8e86da32004b
Michael Leandersson <tripokey@gmail.com>
2018-12-12 21:30:12 +0100

    Moved managed stuff to own module

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,14 @@ use amethyst::{
     renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
     utils::application_root_dir,
 };
-use amethyst::ecs::{Entity, EntityBuilder};
 
-#[derive(Default, Debug)]
-struct ManagedResource {
-    states: Vec<Vec<Entity>>,
-}
+mod managed;
 
-impl ManagedResource {
-    fn push_state(&mut self) {
-        self.states.push(Vec::default());
-    }
+use crate::managed::ManagedWorld;
 
-    fn pop_state(&mut self) -> Vec<Entity> {
-        self.states.pop().expect("There is no active state")
-    }
+struct Example;
 
-    fn create_entity<'a>(&mut self, world: &'a World) -> EntityBuilder<'a> {
-        let builder = world.create_entity_unchecked();
-        self.states.last_mut().expect("There is no active state").push(builder.entity);
-        builder
-    }
-}
-
-struct ManagedState;
-
-impl SimpleState for ManagedState {
+impl SimpleState for Example {
     fn on_start(&mut self, data: StateData<GameData>) {
         data.world.initialize_managed_world();
         data.world.push_state();
@@ -38,32 +20,6 @@ impl SimpleState for ManagedState {
 
     fn on_stop(&mut self, data: StateData<GameData>) {
         data.world.pop_state();
-    }
-}
-
-trait ManagedWorld {
-    fn initialize_managed_world(&mut self);
-    fn create_managed_entity(&self) -> EntityBuilder;
-    fn push_state(&self);
-    fn pop_state(&mut self);
-}
-
-impl ManagedWorld for World {
-    fn initialize_managed_world(&mut self) {
-        self.res.entry::<ManagedResource>().or_insert_with(|| ManagedResource::default());
-    }
-
-    fn create_managed_entity(&self) -> EntityBuilder {
-        self.write_resource::<ManagedResource>().create_entity(self)
-    }
-
-    fn push_state(&self) {
-        self.write_resource::<ManagedResource>().push_state();
-    }
-
-    fn pop_state(&mut self) {
-        let entities = self.write_resource::<ManagedResource>().pop_state();
-        self.delete_entities(&entities).unwrap_or(());
     }
 }
 
@@ -85,7 +41,7 @@ fn main() -> amethyst::Result<()> {
     let game_data =
         GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
 
-    let mut game = Application::new("./", ManagedState, game_data)?;
+    let mut game = Application::new("./", Example, game_data)?;
 
     game.run();
 
---
+++ src/managed.rs
@@ -0,0 +1,49 @@
+use amethyst::prelude::*;
+use amethyst::ecs::{Entity, EntityBuilder};
+
+#[derive(Default)]
+struct ManagedResource {
+    states: Vec<Vec<Entity>>,
+}
+
+impl ManagedResource {
+    fn push_state(&mut self) {
+        self.states.push(Vec::default());
+    }
+
+    fn pop_state(&mut self) -> Vec<Entity> {
+        self.states.pop().expect("There is no active state")
+    }
+
+    fn create_entity<'a>(&mut self, world: &'a World) -> EntityBuilder<'a> {
+        let builder = world.create_entity_unchecked();
+        self.states.last_mut().expect("There is no active state").push(builder.entity);
+        builder
+    }
+}
+
+pub trait ManagedWorld {
+    fn initialize_managed_world(&mut self);
+    fn create_managed_entity(&self) -> EntityBuilder;
+    fn push_state(&self);
+    fn pop_state(&mut self);
+}
+
+impl ManagedWorld for World {
+    fn initialize_managed_world(&mut self) {
+        self.res.entry::<ManagedResource>().or_insert_with(|| ManagedResource::default());
+    }
+
+    fn create_managed_entity(&self) -> EntityBuilder {
+        self.write_resource::<ManagedResource>().create_entity(self)
+    }
+
+    fn push_state(&self) {
+        self.write_resource::<ManagedResource>().push_state();
+    }
+
+    fn pop_state(&mut self) {
+        let entities = self.write_resource::<ManagedResource>().pop_state();
+        self.delete_entities(&entities).unwrap_or(());
+    }
+}
